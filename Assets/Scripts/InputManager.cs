1da6841398fe09dea2020f42dc95df6c5564a9a6
cesarpeixoto <cesars_peixoto@hotmail.com>
2017-05-02 18:25:45 -0300

    Mapeado novas opções de controle do seletor, dispensando o mouse e utilizando as setas do teclado, sendo os atilhos teclas "Q" e "E". Atualizado InputManager com um check para usar ou não o mouse.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,6 +7,7 @@ public class InputManager : MonoBehaviour
     public enum InputDevice { Joystick, keyboardMouse }
     public InputDevice inputDevice = InputDevice.Joystick;
     public float mouseSensitivity = 1.0f;
+    public bool useMouse = false;
     public delegate float AxisHandle();
     public delegate bool ButtonHandle();
 
@@ -18,12 +19,13 @@ public class InputManager : MonoBehaviour
     public static ButtonHandle GetActionTrigger;
     public static ButtonHandle GetCancelTrigger;
 
-    private static float _mouseSensitivity = 1.0f;
+    private static InputManager _instance = null;
+
 
     // Awake is called when the script instance is being loaded
     private void Awake()
     {
-        _mouseSensitivity = mouseSensitivity;
+        _instance = this;
         SetInputDevice(inputDevice);
     }
 
@@ -32,7 +34,7 @@ public class InputManager : MonoBehaviour
         if(controller == InputDevice.Joystick)
         {
             GetPlayerHorizontalAxis = ()=> { return Input.GetAxis("Xbox LHorizontal"); };
-            GetPlayerVerticalAxis = ()=> { return Input.GetAxis("Xbox LVertical"); };
+            GetPlayerVerticalAxis = ()=> { return Input.GetAxis("Xbox LVertical"); };            
             GetSelectorHorizontalAxis = ()=> { return Input.GetAxis("Xbox RHorizontal"); };
             GetSelectorVerticalAxis = () => { return Input.GetAxis("Xbox RVertical"); };
             GetActionTrigger = () => { return Input.GetAxis("Xbox RLTrigger") < 0.0f; };
@@ -42,10 +44,23 @@ public class InputManager : MonoBehaviour
         {
             GetPlayerHorizontalAxis = () => { return Input.GetAxis("Horizontal"); };
             GetPlayerVerticalAxis = () => { return Input.GetAxis("Vertical"); };
-            GetSelectorHorizontalAxis = () => { return Input.GetAxis("Mouse X") * _mouseSensitivity; };
-            GetSelectorVerticalAxis = () => { return Input.GetAxis("Mouse Y") * _mouseSensitivity; };
-            GetActionTrigger = () => { return Input.GetMouseButtonDown(1); };
-            GetCancelTrigger = () => { return Input.GetMouseButtonDown(0); };
+
+            if (_instance.useMouse)
+            {
+                GetSelectorHorizontalAxis = () => { return Input.GetAxis("Mouse X") * _instance.mouseSensitivity; };
+                GetSelectorVerticalAxis = () => { return Input.GetAxis("Mouse Y") * _instance.mouseSensitivity; };
+                GetActionTrigger = () => { return Input.GetMouseButtonDown(1); };
+                GetCancelTrigger = () => { return Input.GetMouseButtonDown(0); };
+            }
+            else
+            {
+                GetSelectorHorizontalAxis = () => { return Input.GetAxis("Selector Horizontal"); };
+                GetSelectorVerticalAxis = () => { return Input.GetAxis("Selector Vertical"); };
+                GetActionTrigger = () => { return Input.GetAxis("Selector Trigger") < 0.0f; };
+                GetCancelTrigger = () => { return Input.GetAxis("Selector Trigger") > 0.0f; };
+            }
+
+            
         }
     }
 
